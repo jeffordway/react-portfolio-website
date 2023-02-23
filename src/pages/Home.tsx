38bc29199541b8f93e6ec981633b5cc159a5ed0b
Jeff Ordway <jeffordway@Jeffs-Macbook-Pro.local>
2023-02-22 17:39:38 -0800

    fixing recent projects

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,6 @@ import { ProjectContext } from "../contexts/ProjectContext";
 
 function Home() {
   const { projects } = useContext(ProjectContext);
-  const recentProjects = projects.slice(-4);
 
   return (
     <div className="flex flex-col gap-12 md:gap-16">
@@ -22,7 +21,7 @@ function Home() {
           <ProjectCards
             className="grid grid-cols-1 gap-6"
             reactLink="projects/"
-            projects={recentProjects}
+            projects={projects.slice(-4)}
           />
         </div>
         <div className="md:col-span-2">
